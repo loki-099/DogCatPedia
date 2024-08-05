30fc93ef64cb356b12e6a5c15a91e9ecafafa9e5
mischiefluis <loidatolentino358@gmail.com>
2024-08-05 21:33:32 +0800

    responsiveness

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
   return (
     <div>
       <img src="/assets/logo.svg" alt="" srcset="" />
-      <div className='flex items-center justify-between mt-28 mx-auto w-full max-w-[335px]'>
-        <LinkMotion whileHover={{ scale: 1.1, rotate: 1}} to={'dogs'} className='w-[155px] relative'><p className='text-4xl absolute left-[50%] -top-4 translate-x-[-50%] bg-transparent choices tracking-wide'>Dogs</p><img src="/assets/dogs.svg" alt="Dogs" className='w-full border-[3px] border-[#1F005F] rounded-[3px]'/></LinkMotion>
-        <LinkMotion whileHover={{ scale: 1.1, rotate: 1}} to={'cats'} className='w-[155px] relative'><p className='text-4xl absolute left-[50%] -top-4 translate-x-[-50%] bg-transparent choices tracking-wide'>Cats</p><img src="/assets/cats.svg" alt="Cats" className='w-full border-[3px] border-[#1F005F] rounded-[3px]'/></LinkMotion>
+      <div className='flex items-center justify-around md:px-8 lg:px-14 mt-28 mx-auto w-full'>
+        <LinkMotion whileHover={{ scale: 1.1, rotate: 1}} to={'dogs'} className='w-[155px] md:w-[230px] lg:w-[285px] relative'><p className='text-4xl md:text-[57px] lg:text-[71px] absolute left-[50%] -top-4 translate-x-[-50%] bg-transparent choices tracking-wide'>Dogs</p><img src="/assets/dogs.svg" alt="Dogs" className='w-full border-[3px] border-[#1F005F] rounded-[3px]'/></LinkMotion>
+        <LinkMotion whileHover={{ scale: 1.1, rotate: 1}} to={'cats'} className='w-[155px] md:w-[230px] lg:w-[285px] relative'><p className='text-4xl md:text-[57px] lg:text-[71px] absolute left-[50%] -top-4 translate-x-[-50%] bg-transparent choices tracking-wide'>Cats</p><img src="/assets/cats.svg" alt="Cats" className='w-full border-[3px] border-[#1F005F] rounded-[3px]'/></LinkMotion>
       </div>
     </div>
   )
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const Root = () => {
   return (
-    <div className='mx-auto w-[89%] max-w-[400px] pt-4'>
+    <div className='mx-auto w-[89%] max-w-[400px] md:max-w-[650px] lg:max-w-[865px] pt-4 md:pt-6'>
       <Outlet/>
     </div>
   )
